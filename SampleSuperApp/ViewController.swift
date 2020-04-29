c69577c70d7065adb9adcf8a08f27c952cd3cfbf
azamat <azamat.kushmanov@gmail.com>
2020-04-29 15:09:35 +0600

    Initial Commit

---
+++ SampleSuperApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleSuperApp
+//
+//  Created by azamat on 4/29/20.
+//  Copyright © 2020 azamat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
