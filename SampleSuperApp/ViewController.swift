4d0a711fb1cc45039a3e2bfe7d894829382121e4
Azamat Kushmanov <azamat.kushmanov@gmail.com>
2020-09-28 14:30:37 +0600

    updated to 1.3.32 version

--- SampleSuperApp/ViewController.swift
+++ SampleSuperApp/ViewController.swift
@@ -17,44 +17,44 @@ class ViewController: UIViewController {
     
         NotificationCenter.default.addObserver(self, selector: #selector(handlePushNotification), name: .onPushNotificationTapped, object: nil)
         
-        AppBoxo.shared.setConfig(config: Config(clientId: "352131"))
+        Appboxo.shared.setConfig(config: Config(clientId: "352131"))
         handlePushNotification()
     }
 
     @IBAction func openDemo(_ sender: Any) {
-        let miniApp = AppBoxo.shared.getMiniApp(appId: "app16973", authPayload: "payload", data: "data")
-        //miniApp.delegate = self
-        miniApp.open(viewController: self)
+        let miniapp = Appboxo.shared.getMiniapp(appId: "app16973", authPayload: "payload")
+        miniapp.delegate = self
+        miniapp.open(viewController: self)
     }
     
     @IBAction func openSkyscanner(_ sender: Any) {
-        let miniApp = AppBoxo.shared.getMiniApp(appId: "app85076", authPayload: "payload", data: "data")
-        //miniApp.delegate = self
-        miniApp.open(viewController: self)
+        let miniapp = Appboxo.shared.getMiniapp(appId: "app85076", authPayload: "payload")
+        //miniapp.delegate = self
+        miniapp.open(viewController: self)
     }
     
     @IBAction func openStore(_ sender: Any) {
-        let miniApp = AppBoxo.shared.getMiniApp(appId: "app36902", authPayload: "payload", data: "data")
-        miniApp.delegate = self
-        miniApp.open(viewController: self)
+        let miniapp = Appboxo.shared.getMiniapp(appId: "app36902", authPayload: "payload")
+        miniapp.delegate = self
+        miniapp.open(viewController: self)
     }
     
     @objc func handlePushNotification() {
         DispatchQueue.main.async { [weak self] in
             guard let self = self else { return }
             
-            if let push = UserDefaults.standard.value(forKey: "Notification") as? [String : Any], let miniAppId = push["miniapp_id"] as? String {
+            if let push = UserDefaults.standard.value(forKey: "Notification") as? [String : Any], let miniappId = push["miniapp_id"] as? String {
                 UserDefaults.standard.setValue(nil, forKey: "Notification")
-                let miniApp = AppBoxo.shared.getMiniApp(appId: miniAppId, authPayload: "", data: "data")
-                miniApp.open(viewController: self)
+                let miniapp = Appboxo.shared.getMiniapp(appId: miniappId, authPayload: "")
+                miniapp.open(viewController: self)
             }
         }
     }
 }
 
-extension ViewController : MiniAppDelegate {
-    func didReceiveCustomEvent(miniApp: MiniApp, params: [String : Any]) {
-        guard miniApp.appId == "app36902" else { return }
+extension ViewController : MiniappDelegate {
+    func didReceiveCustomEvent(miniapp: Miniapp, params: [String : Any]) {
+        guard miniapp.appId == "app36902" else { return }
         
         
         let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
@@ -72,35 +72,35 @@ extension ViewController : MiniAppDelegate {
         }
     }
     
-    func onLaunch(miniApp: MiniApp) {
-        print("onLaunchMiniApp: \(miniApp.appId)")
+    func onLaunch(miniapp: Miniapp) {
+        print("onLaunchMiniapp: \(miniapp.appId)")
     }
     
-    func onResume(miniApp: MiniApp) {
-        print("onResumeMiniApp: \(miniApp.appId)")
+    func onResume(miniapp: Miniapp) {
+        print("onResumeMiniapp: \(miniapp.appId)")
     }
     
-    func onPause(miniApp: MiniApp) {
-        print("onPauseMiniApp: \(miniApp.appId)")
+    func onPause(miniapp: Miniapp) {
+        print("onPauseMiniapp: \(miniapp.appId)")
     }
     
-    func onClose(miniApp: MiniApp) {
-        print("onCloseMiniApp: \(miniApp.appId)")
+    func onClose(miniapp: Miniapp) {
+        print("onCloseMiniapp: \(miniapp.appId)")
     }
     
-    func onError(miniApp: MiniApp, message: String) {
-        print("onErrorMiniApp: \(miniApp.appId) message: \(message)")
+    func onError(miniapp: Miniapp, message: String) {
+        print("onErrorMiniapp: \(miniapp.appId) message: \(message)")
     }
 }
 
 extension ViewController : CheckoutViewControllerDelegate {
     func didSuccessPayment(vc: CheckoutViewController, params: [String : Any]) {
-        guard let miniApp = AppBoxo.shared.getMiniApp(appId: "app36902") else { return }
+        guard let miniapp = Appboxo.shared.getMiniapp(appId: "app36902") else { return }
         
         var newParams = params
         newParams["payload"] = ["payment":"received"]
         
-        miniApp.sendEvent(params: params)
+        miniapp.sendEvent(params: params)
     }
 }
 
